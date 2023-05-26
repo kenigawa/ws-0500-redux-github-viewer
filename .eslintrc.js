e4010697cb3f22f7190bfef1112bf169cf8382ff
Ken Igawa <kenigawa@KennoMacBook-Air.local>
2023-05-27 06:54:56 +0900

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "prettier/prettier": [
+            "error",
+            {"singleQuote": true}
+        ]
+    },
+    "globals": {
+        "document": true
+    }
+}
